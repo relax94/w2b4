d2bcca8f37b05f4bc7c65ecfe31072fe5a327506
relax94 <basket.pavlenko@gmail.com>
2017-02-06 16:54:09 +0200

    small improvements

--- webs/lib/routes/NetworkRoutes.js
+++ webs/lib/routes/NetworkRoutes.js
@@ -14,6 +14,7 @@ import { NN, NeuronType } from './nn'
 //     }
 // };
 
+
 const N = new NN({
     layers: [{
         type: NeuronType.INPUT,
@@ -25,42 +26,55 @@ const N = new NN({
     },
     {
         type: NeuronType.OUTPUT,
-        size: 5
+        size: 10
     }]
 });
 
+N.deserializeFromFile("d://network.txt");
 
-N.trainNetwork([
+/*N.trainNetwork([
+    [1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1],// 0
     [0, 1, 1, 1, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1], //1
     [1, 1, 1, 0, 0, 1, 0, 1, 0, 1, 0, 0, 1, 1, 1], //2
     [1, 1, 1, 0, 0, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1], //3
     [1, 0, 1, 1, 0, 1, 1, 1, 1, 0, 0, 1, 0, 0, 1], // 4
-    [1, 1, 1, 1, 0, 0, 1, 1, 1, 0, 0, 1, 1, 1, 1] // 5
+    [1, 1, 1, 1, 0, 0, 1, 1, 1, 0, 0, 1, 1, 1, 1],// 5
+    [1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 0, 1, 1, 1, 1], //6
+    [1, 1, 1, 0, 0, 1, 0, 1, 0, 0, 1, 0, 1, 0, 0],// 7
+    [1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1],// 8
+    [1, 1, 1, 1, 0, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1]// 9
 ], [
-        [1, 0, 0, 0, 0],
-        [0, 1, 0, 0, 0],
-        [0, 0, 1, 0, 0],
-        [0, 0, 0, 1, 0],
-        [0, 0, 0, 0, 1]
-    ], () => {
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 1, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 1, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 1, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 1, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 1, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 1, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 1, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 1]
+   ], () => {
         console.log('TRAIN SUCCESS');
+        N.serializeToFile();
         // N.run([1, 0, 1, 1, 0, 1, 1, 1, 1, 0, 0, 1, 0, 0, 1]);
-    });
+    });*/
 
 
 
 export let PostImageData = {
+    config: {
+        cors: {
+            origin: ['*'],
+            additionalHeaders: ['cache-control', 'x-requested-with']
+        }
+    },
     method: 'POST',
     path: '/network/games/image',
     handler: (req, reply) => {
         for (let k in req.payload)
-            N.run(req.payload[k]);
-
-        console.log('');
-        console.log('');
-        console.log('');
-        console.log('');
-
-        return reply();
+            N.run(req.payload[k], function (output) {
+                return reply(output);
+            });
     }
 };
--- webs/lib/routes/nn.js
+++ webs/lib/routes/nn.js
@@ -1,3 +1,5 @@
+import {appendFile, readFile} from 'fs'
+
 export const NeuronType = {
     NONE: "NONE",
     INPUT: "INPUT",
@@ -98,13 +100,17 @@ export class NN {
     }
 
     networkPreInit() {
+        this.initCore();
+        for (let layer of this.config.layers)
+            this.createLayer(layer);
+    }
+
+    initCore(){
         this.inputSize = this.config.layers.find(l => l.type === NeuronType.INPUT).size;
         this.hiddenSize = this.config.layers.find(l => l.type === NeuronType.HIDDEN).size;
         this.outputSize = this.config.layers.find(l => l.type === NeuronType.OUTPUT).size;
 
         this.network = [];
-        for (let layer of this.config.layers)
-            this.createLayer(layer);
     }
 
     createLayer(layer) {
@@ -128,7 +134,7 @@ export class NN {
     computeNeuronsValue(neuronType, linkType, fn, print) {
         return this.network.filter(h => h.type === neuronType).forEach((h) => {
             h.computeValue(this.references.filter(r => r.type === linkType && r.neuronToKey === h.key)
-                .reduce((prev, curr) => (prev + (curr.weight * fn(curr))), 0) + h.bias);
+                    .reduce((prev, curr) => (prev + (curr.weight * fn(curr))), 0) + h.bias);
 
             if (neuronType === NeuronType.OUTPUT)
                 if (print)
@@ -172,7 +178,7 @@ export class NN {
 
         this.learnCycles = 0;
 
-        while (this.learnCycles < 2000) {
+        while (this.learnCycles < 1000) {
 
             this.learnCycles++;
             for (let randomSet = 0; randomSet < trainInputs.length; randomSet++) {
@@ -184,23 +190,58 @@ export class NN {
                 this.adjustInputs((ref) => trainInputs[randomSet][ref.neuronFromKey]);
             }
 
-            if (this.learnCycles === 2000)
+            if (this.learnCycles === 1000)
                 return fn();
 
+            if (this.learnCycles % 100 === 0)
+                console.log('LEARN CYCLES ', this.learnCycles);
         }
 
     }
 
-    run(data) {
-
+    run(data, outputFn) {
+        let output = [];
         this.computeNeuronsValue(NeuronType.HIDDEN, LinkType.INPUT_TO_HIDDEN, (ref) => data[ref.neuronFromKey]);
         this.computeNeuronsValue(NeuronType.OUTPUT, LinkType.HIDDEN_TO_OUTPUT, (curr) => this.network.find(n => n.type === NeuronType.HIDDEN && n.key === curr.neuronFromKey).value, (value) => {
 
-            console.log('OUT ', value);
+            output.push(value);
+            if (output.length === this.outputSize)
+                outputFn(output);
+            // console.log('OUT ', value);
         });
-
-
     }
 
+    networkStringConfiguration() {
+        const networkSaveConfig = {
+            configuration: this.config,
+            network: this.network,
+            references: this.references,
+            learnCycles: this.learnCycles
+        };
+
+        return JSON.stringify(networkSaveConfig);
+    }
+
+    serializeToFile() {
+        appendFile("d://network.txt", this.networkStringConfiguration(), (err) => {
+            console.log('append err ', err);
+        });
+    }
+
+    deserializeFromFile(path) {
+        readFile(path, "utf8", (err, content) => {
+            if (!err) {
+                this.deserializeObj = JSON.parse(content);
+                this.config = this.deserializeObj.configuration;
+                this.network = this.deserializeObj.network;
+                this.references = this.deserializeObj.references;
+                this.learnCycles = this.deserializeObj.learnCycles;
+
+                console.log('SUCCESS DESEREALIZING', this.learnCycles);
+            }
+            else
+                console.log('FAILED DESERIALIZE NETWORK FILE');
+        });
+    }
 }
 
